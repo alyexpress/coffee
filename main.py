a4219229e0acc57a2597884520ae8e3c1e64ae04
Igor Alymov <al.ymov@yandex.ru>
2024-12-01 02:06:59 +0300

    addEditCoffeeForm.ui add

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sqlite3
 
 from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication
-from PyQt6.QtWidgets import QMainWindow, QTableWidgetItem
+from PyQt6.QtWidgets import QMainWindow, QWidget, QTableWidgetItem
 
 
 class MyWidget(QMainWindow):
@@ -12,6 +12,15 @@ class MyWidget(QMainWindow):
         uic.loadUi('main.ui', self)
         self.db = sqlite3.connect('coffee.sqlite')
         self.cur = self.db.cursor()
+        self.loadTable()
+        self.nextForm.clicked.connect(self.goToNextForm)
+
+    def goToNextForm(self):
+        self.next = AddCoffee(self)
+        self.next.show()
+
+    def update(self):
+        self.tableWidget.clearContents()
         self.loadTable()
 
     def loadTable(self):
@@ -29,6 +38,35 @@ class MyWidget(QMainWindow):
                 self.tableWidget.setItem(
                     i, j, QTableWidgetItem(elem))
         self.tableWidget.resizeColumnsToContents()
+
+class AddCoffee(QWidget):
+    def __init__(self, ms):
+        super().__init__()
+        self.ms = ms
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.db = sqlite3.connect('coffee.sqlite')
+        self.cur = self.db.cursor()
+        self.save.clicked.connect(self.click)
+
+    def click(self):
+        name, roasting, _type, description, price, size = (
+            self.name.text(), self.roasting.value(),
+            self.type.currentText(), self.description.toPlainText(),
+            self.price.value(), self.size.value())
+        query = "SELECT id FROM coffee WHERE name = ?"
+        if self.cur.execute(query, (name,)).fetchall():
+            query = """UPDATE coffee SET roasting = ?, type = ?,
+            description = ?, price = ?, size = ? WHERE name = ?"""
+            self.cur.execute(query, (roasting, _type, description,
+                                     price, size, name))
+        else:
+            query = """INSERT INTO coffee(name, roasting, type,
+            description, price, size) VALUES (?, ?, ?, ?, ?, ?)"""
+            self.cur.execute(query, (name, roasting, _type,
+                                     description, price, size))
+        self.db.commit()
+        self.ms.update()
+        self.close()
 
 
 if __name__ == '__main__':
